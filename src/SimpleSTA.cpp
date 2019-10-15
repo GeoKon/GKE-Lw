fdec93a0ffe0e9bf33ff8c10ca8857e0d4829514
George Kontopidis <KONTOPIDIS@IEEE.ORG>
2019-10-14 22:48:12 -0400

    Virtually re-written to handle WiFi() disconnects and reconnects in a non-blocking fashion

--- src/SimpleSTA.cpp
+++ src/SimpleSTA.cpp
@@ -1,16 +1,57 @@
-// -------- Minimum includes for this module --------------------------------------
+// -------- Choose one of the two options below ---------------------------------
 
+//  #define MULTI_WIFI		
+#undef  MULTI_WIFI
+
+// -------------------------------------------------------------------------------
+
+    #include <ESP8266WiFi.h> 
+	#define TEST_CONNECTION WiFi.status()
+	#define SETUP_WIFI(A,B) WiFi.begin(A,B)
     
     #include <ESP8266mDNS.h>        // Include the mDNS library
 
     #include "externIO.h"           // global externals of cpu,...,server
-    #include "SimpleSTA.h"          // exported functions by this file    
+    #include "ledClass.h"
+	#include "SimpleSTA.h"          // exported functions by this file    
+	
+LED led;							// allocate the LED structure
 
 // ----------------------- Console interface ----------------------------------------
+
+bool startCLIAfter( int timeout, BUF *bp )
+{
+	cli.init( ECHO_ON, "cmd: " );    
+	
+	PFN("Press RETURN within %d sec to start CLI", timeout );
+	
+	led.pin( cpu.getLedPin() );
+	led.pattern( WAITFOR_CLI, timeout);
+	
+	while( led.blink( Serial.read() != 0x0D ) )
+	{
+		if( cpu.buttonPressed() )
+		{
+			led.stop();						// stop blinking
+			return true;					
+		}
+	}	
+	if( led.expired() )
+		return false;
+	
+	if( led.aborted() )
+	{
+		cpu.led( ON );
+		interactForever( bp );
+		cpu.led( OFF );
+		PRN("Continuing Setup()...");      // break the loop if 'exit' is entered 
+		return false;
+	}
+}
+
 void interactForever( BUF *bp )
 {
     PF("\r\nCLI Mode. Press 'h' for help\r\n");
-    cpu.led( ON );
     
     cli.prompt();
     
@@ -33,86 +74,171 @@ void interactForever( BUF *bp )
         }
     }
 }
-bool startCLIAfter( int timeout, BUF *bp )
+
+// --------------------- new WiFi connect with Smart Connect -----------------------
+
+bool isWiFiConnected()
 {
-	cli.init( ECHO_ON, "cmd: " );       
-
-	PF("Press RETURN to start CLI within %d sec\r\n", timeout );
-
-	for( int i=0; i< timeout*100; i++ ) 	// 100 loops per second
-	{
-		cpu.toggleEvery( 100 );             // fast blink to indicate waiting for RETURN
-		if( (i%100)==0 ) 
-			PR(".");
-		if( Serial.read() == 0x0D )
-		{
-			CRLF();
-			interactForever( bp );
-			PRN("Continuing Setup()...");
-			break;                          // break the loop if 'exit' is entered    
-		}
-		if( cpu.button() )
+    if( WiFi.status() == WL_CONNECTED) 
+    {
+        if( WiFi.localIP() != INADDR_NONE )
             return true;
-		delay( 10 );
-	} 
-	CRLF();	
-	return false;
-}
+    }
+    return false;
+} 
 
-// ----------------------- Exported functions -------------------------------------
-
-bool setupSTA( int tmout )
+void setupWiFi( char *ssid, char *pwd, char *staticIP )
 {
-    byte mac[6];
+	// Force clearing the previous settings
+	// see: https://circuits4you.com/2018/03/09/esp8266-static-ip-address-arduino-example/
 	
-	PR("\r\nSTA mode...\r\n\tMAC=");
-	PRN( WiFi.macAddress() );                   // mac is used later to define mDNS
+	WiFi.begin( ssid, pwd ); 
 	
-	WiFi.begin( eep.wifi.ssid, eep.wifi.pwd );	// see: https://circuits4you.com/2018/03/09/esp8266-static-ip-address-arduino-example/
-	WiFi.disconnect();							// 
-	
-    char *staticIP = eep.wifi.stIP;
-    if( *staticIP )
+	WiFi.disconnect( /*true*/ );	// use this to clear past connections
+	if( *staticIP )
     {
         IPAddress ip, gateway(192,168,0,1), subnet( 255,255,255,0 ), dns(8, 8, 8, 8);
         ip.fromString( staticIP );
-		PR( "See IP="); PR( staticIP ); PR(" "); PRN( ip );
+        PR( " with static IP="); PRN( ip );
         WiFi.config( ip, gateway, subnet, dns );
-
     }
-    WiFi.begin( eep.wifi.ssid, eep.wifi.pwd );
-    WiFi.mode(WIFI_STA);
+   	WiFi.begin( ssid, pwd ); 
+  	WiFi.mode(WIFI_STA);
+  	
+	PFN("Attempting to connect to SSID:%s with PWD:%s", ssid, pwd );
 	
-    bool ok = false;
-	for( int i=0; (i<tmout*2)||(tmout==0); i++  )
-    {
-        if( WiFi.status() == WL_CONNECTED )
-        {
-            PF( "Connected to %s. Hostname is %s. ", WiFi.SSID().c_str(), WiFi.hostname().c_str() );
-            PR( "IP Address " ); PRN( WiFi.localIP() );
-            ok = true;
-            break;
-        }
-        delay(250);
-        if( i%2 ) cpu.led( ON );
-        else cpu.led( OFF );
-    }    
-    // BUF name(16);
-    // name.set("GKELAB%d", eep.head.reboots );
-    // if (!MDNS.begin( !name )) 
-        // cpu.die("Error setting up MDNS responder!", 3 );
-    // MDNS.addService("GKETCP", "tcp", eep.wifi.port );
-    // MDNS.addService("GKEUDP", "udp", 5353);
-    // PF("mDNS responder started. Ping for %s.local:%d\r\n", !name, eep.wifi.port); 
+	bool dosmart = false;
+	led.pin( cpu.getLedPin() );
+	led.pattern( WAITFOR_WIFI, 20 );						// 20 second timeout
 	
+	while( led.blink( WiFi.status() != WL_CONNECTED ) )
+	{
+		if( cpu.button() )									// when interrupting the loop, 
+		{													//  need to stopLED
+			led.stop();
+			dosmart = true;
+			break;
+		}
+	}
+	if( dosmart || led.expired() )
+  	{
+    	PRN("Attempting Smart Config" );	
+        led.pattern( WAITFOR_SMARTCONFIG );					// wait forever
+        WiFi.beginSmartConfig();
+       
+        while( led.blink( !WiFi.smartConfigDone() ))		
+       		;
+		ASSERT( led.aborted() );							// Smart config OK
+		strcpy( eep.wifi.ssid, WiFi.SSID().c_str() );
+		strcpy( eep.wifi.pwd,  WiFi.psk().c_str() );
+		eep.saveWiFiParms();
+		PRN("Smartconf OK. WiFi Credentials saved in EEPROM");
+    }
+    if( led.aborted() )										// connection successful
+		;
+	
+	PFN( "Hostname:%s, SSID:%s, PWD:%s, RSSI:%ddBm", 
+		WiFi.hostname().c_str(), 
+		WiFi.SSID().c_str(), 
+		WiFi.psk().c_str(), 
+		WiFi.RSSI() );
+
+  	//WiFi.printDiag(Serial);
+
+  	PRN("Getting IP Address" );			
+  	led.pattern( WAITFOR_IPADDR );							// wait for ever 
+  	while( led.blink(WiFi.localIP() == INADDR_NONE) )
+  		;
+	ASSERT( led.aborted() );								
+	PR( "IP Address: " ); PRN( WiFi.localIP() );
+	
+	PRN( "Connected OK" );
+	ASSERT( isWiFiConnected() );
+}
+/* 
+Use the following to test:
+   1. start application with valid SSID. If not, it will do SmartConfig()
+   2. in the main loop change SSID to invalid
+   3. do a 'disconnect'; this will start the checkWiFi() loop
+   4. ensure that switch ON/OFF work OK
+   5. set a valid SSID; reconnection should happen.
+*/
+
+static int state = 0;			
+/* 	
+	Shared variable between checkWiFi() and reconnectWiFi()
+
+	0 = idle state. WiFi is connected.
+	1 = just disconnected. Start reconnecting sequence.
+	2 = test if now connected. Wait until connection is OK
+*/
+bool checkWiFi()
+{
+	if( isWiFiConnected() )
+	{
+		if( state != 0 )		// this indicates that reconnectWiFi() was connected OK
+		{
+			PF("\r\nPrev state %d. Reconnected with IP=", state );
+			Serial.println(WiFi.localIP());
+		}
+		state = 0;
+		return true;
+	}	
+	else						// WiFi is not connected
+	{
+		if( state == 0 )		// If in idle state,
+		{
+			state = 1;			//  start the reconnect sequence
+			CRLF();
+		}	
+		return false;
+	}
+}	
+void reconnectWiFi( void (*cb)(), const char *pattern )
+{
+	switch( state )
+	{
+		default:
+		case 0:										// do nothing
+			break;
+		
+		case 1: 
+			WiFi.begin( eep.wifi.ssid, eep.wifi.pwd );  
+		  	WiFi.mode(WIFI_STA);
+			PFN("Attempting to reconnect to SSID:%s PWD:%s", 
+				eep.wifi.ssid, eep.wifi.pwd );
+			led.pattern( pattern, 10 );				// with 10 sec timeout			
+			state = 2;
+			break;
+		case 2:
+			if( cb )								// callback if not null
+				(*cb)();
+			if( led.blink( !isWiFiConnected() )	)	// stay in this state if not connected
+				break;
+		  	if( led.expired() )						// repeat the attempt in 10sec
+				state = 1;
+		  	else //if( led.aborted() )				// WiFi is connected here
+		  	{
+				PR("Reconnected. IP Address is ");
+				Serial.println(WiFi.localIP());
+				state = 0;
+			}
+			break;									// reconnection is done
+	}
+}
+
+bool startMDNS()
+{
+	byte mac[6];
 	B64( name );
 	WiFi.macAddress( mac );
 	name.set("GKE%02X%02X", mac[0], mac[5] );     // MSB and LSB of mac address
-	ASSERT( MDNS.begin( !name ) ) ;
-	PF("mDNS advertising: %s.local:%d\r\n", name.c_str(), eep.wifi.port ); 
-    
-    cpu.led( BLINK, 2 );            // indicates successful WiFi connection 
-	return ok;
+	bool ok = MDNS.begin( !name ) ;
+	if( ok )
+		PF("mDNS advertising: %s.local:%d\r\n", name.c_str(), eep.wifi.port ); 
+	else
+		PF("mDNS could not start!\r\n" );
+	return ok;	
 }
 
 void loopSTA()
@@ -124,32 +250,3 @@ void loopSTA()
         cli.prompt();                
     }      
 }
-
-//#include <ESP8266HTTPClient.h>
-//HTTPClient http;                    // Object of class HTTPClient
-//void loopSTA()
-//{
-//    static bool pending = false;
-//    
-//    if( cli.ready() )               // handle serial interactions
-//    {
-//        //exe.dispatch( cli.gets() );
-//        char *p = cli.gets();
-//        http.begin( p );            // "http://192.168.0.70:8067/status");
-//        pending = true;
-//    }
-//    if( pending )
-//    {
-//        int httpCode = http.GET();
-//        if (httpCode > 0) 
-//        {
-//            String buffer;
-//            buffer = http.getString();
-//            PF("Received: %s\r\n", !buffer );
-//            pending = false;
-//            http.end(); //Close connection
-//            cli.prompt();
-//        }
-//    }
-//    server.handleClient();    
-//}
--- src/SimpleSTA.h
+++ src/SimpleSTA.h
@@ -1,13 +1,42 @@
 #pragma once
+#include <bufClass.h>
+#include <externIO.h>
 
-// ------ Expected definitions/allocations in main() ------------------------------
-  
-// Exported functions by ExampleSTA.cpp
+enum recon_t
+{
+	INITIALIZE = 0,
+	RECONNECT = 1,
+	WIFI_OK = 2
+};
+
+#define WAITFOR_CLI 		"$*."
+#define WAITFOR_WIFI 		"$*...."
+#define WAITFOR_SMARTCONFIG "$*..*........"
+#define WAITFOR_IPADDR		"$*****.."
+#define WAITFOR_RECONN		"$*......."
+
+// CLI Exported functions 
 
 	bool startCLIAfter( int timeoutsec, BUF *bp=NULL );
-    void interactForever( BUF *bp=NULL );
+    void interactForever( BUF *bp=NULL );  // requires cli.init();
+
+// WiFi Connections (use these)
 	
-    bool setupSTA( int tmout = 20 );
+	bool isWiFiConnected();
+	void setupWiFi( char *ssid=eep.wifi.ssid, 
+					char *pwd=eep.wifi.pwd, 
+					char *staticIP=eep.wifi.stIP );
+
+	bool checkWiFi();
+	void reconnectWiFi( void (*cb)()=NULL,
+					    const char *pattern=WAITFOR_RECONN);				
+	bool startMDNS();
+
     void loopSTA();
-	
-	
+
+// WiFi Connections	(deprecated)
+    // bool setupSTA( int tmout = 20 );
+	// bool connectToNetwork( int tmout, void (*func)() = [](){delay(250);} );
+	// bool printOnce( char *s );				// use NULL to initialize. Include in a loop
+	// void indicateDisconnect( bool doit );  	// blinks LED if WiFi disconnected
+
